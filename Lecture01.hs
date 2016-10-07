67b99b2541339d1e4afab9b6e0355fb197c85842
Christoph Mende <mende.christoph@gmail.com>
2016-10-07 11:46:47 +0200

    Add missing content from lecture 01

--- Lecture01.hs
+++ Lecture01.hs
@@ -1,3 +1,4 @@
+module Lecture01 where
 {-# OPTIONS_GHC -w #-}
 
 main :: IO ()
@@ -12,6 +13,36 @@ comment
 
 magicNumber :: Int
 magicNumber = 1
+-- magicNumber = 2
+
+i :: Integer
+i = 1234123134141234123412341234213434
+
+d :: Double
+d = 1.2
+
+typeError = fromIntegral i + d
+
+b1 :: Bool
+b1 = False
+
+b2 :: Bool
+b2 = True
+
+c :: char
+c = '\n'
+
+ex5 = mod 4 5
+
+ex9 = False || True
+
+ex11 = 'a' /= 'b'
+
+ex12 = "ab" <= "b"
+
+ex13 = 3 + 4 * 6
+
+ex14 = if 3 < 5 then 4 else 5
 
 -- Input -> Output
 fac :: Integer -> Integer
