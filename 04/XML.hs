5ac1fbf09feddbb7330870d5e18f03b62b6f086c
Christoph Mende <mende.christoph@gmail.com>
2016-10-21 19:01:47 +0200

    Implement foldXML

--- 04/XML.hs
+++ 04/XML.hs
@@ -4,17 +4,19 @@ data XML = XText String
          | XNode String [Attr] [XML]
 data Attr = String := String
 
--- foldXML :: (String -> a) -> (String -> [Attr] -> [a] -> a) -> XML -> a
--- foldXML 
+foldXML :: (String -> a) -> (String -> [Attr] -> [a] -> a) -> XML -> a
+foldXML f _ (XText s) = f s
+foldXML _ f (XNode s as []) = f s as []
+foldXML f1 f2 (XNode s as xs) = f2 s as (map (foldXML f1 f2) xs)
+
 
 attrToString :: Attr -> String
 attrToString (a := b) = a ++ "='" ++ b ++ "'"
 
 xmlToString :: XML -> String
-xmlToString (XText x)            = x
-xmlToString (XNode x attrs xmls) =
-  "<"++ unwords (x : map attrToString attrs) ++">"
-  ++ concatMap xmlToString xmls ++"</"++x++">"
+xmlToString = foldXML id
+  (\ s as ss ->
+    "<" ++ unwords (s : map attrToString as) ++ ">" ++ concat ss ++"</"++s++">")
 
 xml :: XML
 xml = XNode "foo" ["x" := "y"] [XNode "bar" [] [XText "test"]]
