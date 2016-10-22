269450cc93937dd8ddfe43c841505e843af235b0
Christoph Mende <mende.christoph@gmail.com>
2016-10-22 13:49:45 +0200

    Initial implementation of RNA3

---
+++ 04/RNA3.hs
@@ -0,0 +1,25 @@
+module RNA3 where
+
+import RNA (Base(..), RNA)
+
+parseBase :: Char -> Either String Base
+parseBase 'U' = Right U
+parseBase 'A' = Right A
+parseBase 'C' = Right C
+parseBase 'G' = Right G
+parseBase x = Left ("No base: " ++ [x])
+
+foldEither :: (a -> b) -> Either b a -> b
+foldEither _ (Left x)  = x
+foldEither f (Right x) = f x
+
+(>>-) :: Either c a -> (a -> Either c b) -> Either c b
+(>>-) (Right x) f = foldEither f (Right x)
+(>>-) (Left x) _  = Left x
+
+result :: a -> Either c a
+result = Right
+
+parseRNA :: String -> Either String RNA
+parseRNA []     = Right []
+parseRNA (c:cs) = parseRNA cs >>- (\ x -> parseBase c >>- (\ y -> result (y:x)))
