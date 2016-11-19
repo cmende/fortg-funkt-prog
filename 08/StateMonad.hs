f208bf9160ea6b6f7496250f7942dfcc85ceb6ed
Christoph Mende <mende.christoph@gmail.com>
2016-11-19 13:26:47 +0100

    Move state monad code to new file

---
+++ 08/StateMonad.hs
@@ -0,0 +1,37 @@
+module StateMonad where
+
+import           Control.Monad (ap)
+
+
+data Tree a = Empty
+            | Node (Tree a) a (Tree a)
+            deriving Show
+
+numberTree' :: Tree a -> Int -> (Tree Int,Int)
+numberTree' Empty x = (Empty,x)
+numberTree' (Node l _ r) x =
+  let
+    (tl,xl) = numberTree' l (x + 1)
+    (tr,xr) = numberTree' r xl
+  in (Node tl x tr,xr)
+
+tree1 :: Tree String
+tree1 = Node (Node Empty "a" (Node Empty "b" Empty)) "c" (Node (Node Empty "d" Empty) "e" Empty)
+
+
+data State s a = State { runState :: s -> (a,s) }
+
+instance Functor (State s) where
+  -- fmap :: (a -> b) -> State s a -> State s b
+  fmap f (State g) = State (\ s -> let (a,s2) = g s in (f a,s2))
+
+instance Monad (State s) where
+  -- return :: a -> State s a
+  return x = State (\ s -> (x,s))
+
+  -- (>>=) :: State s a -> (a -> State s b) -> State s b
+  (State f) >>= k = State (\ s -> let (a,s2) = f s in runState (k a) s2)
+
+instance Applicative (State s) where
+ pure = return
+ (<*>) = ap
